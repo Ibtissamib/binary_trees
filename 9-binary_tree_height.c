bab46dd1bf7edeb5620e92e46e8da58eb60492a2
Ibtissam <ibboutayeb@gmail.com>
2024-06-11 15:04:14 +0000

    9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+# include "binary_trees.h"
+/**
+  *binary_tree_height- measures the height of a binary tree
+  *@tree: const binary_tree_t *
+  *Return: size_t
+  */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t r, l;
+
+	if (tree == NULL)
+		return (0);
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+	if (l > r)
+		return (l + 1);
+	else
+		return (r + 1);
+}
