97dadbc516125b291df7ee4175e7c9a76ecbf114
Ibtissam <ibboutayeb@gmail.com>
2024-06-11 23:19:54 +0000

    11

--- binary_trees.h
+++ binary_trees.h
@@ -38,4 +38,5 @@ size_t bl(const binary_tree_t *tree);
 size_t br(const binary_tree_t *tree);
 size_t inc(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 #endif
