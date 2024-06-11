09a588c9af5849c8afc2cc716173db5fe821727b
Ibtissam <ibboutayeb@gmail.com>
2024-06-11 17:43:31 +0000

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,26 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l, r;
+
+	l = bl(tree->left);
+	r = br(tree->right);
+	if (l > r)
+		return (l);
+	else
+		return (r);
+}
+size_t bl(const binary_tree_t *tree)
+{
 	if (tree == NULL)
 		return (0);
-	l = 1 + binary_tree_height(tree->left);
-	r = 1 + binary_tree_height(tree->right);
-	if (l > r)
-		return (l - 1);
 	else
-		return (r - 1);
+		return (1 + bl(tree->left));
 }
+size_t br(const binary_tree_t *tree, size_t l)
+{
+	if (tree == NULL)
+		return (0);
+	else
+		return (1 + bl(tree->right));
+}
+
