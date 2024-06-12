e5bfe353261ecddfd3c2563168896477469a05e5
Ibtissam <ibboutayeb@gmail.com>
2024-06-12 01:55:45 +0000

    15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,16 @@
+# include "binary_trees.h"
+/**
+  *binary_tree_is_full- if tree is full
+  *@tree: btree
+  *Return: int
+  */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
